a52c32ef1fb77945bff156962ccef0c4911b5a41
mederitus <Jhgf@9510>
2021-10-02 11:12:01 -0300

    medeiros 02102021 11:08

--- src/main/java/com/betha/notafacil/model/Bairro.java
+++ src/main/java/com/betha/notafacil/model/Bairro.java
@@ -1,4 +1,23 @@
 package com.betha.notafacil.model;
 
 public class Bairro {
+
+    private Integer id;
+    private String nome;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
--- src/main/java/com/betha/notafacil/model/Usuario.java
+++ src/main/java/com/betha/notafacil/model/Usuario.java
@@ -1,4 +1,8 @@
 package com.betha.notafacil.model;
 
 public class Usuario {
+
+    private String login;
+    private String password;
+
 }
